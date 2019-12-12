666b4a935f199c92febacfaf5630ee10eb373838
Quintis1212 <34488718+Quintis1212@users.noreply.github.com>
2019-12-12 18:10:47 +0200

    Add files via upload

---
+++ build/precache-manifest.3e46c7a1dfaed7d01fe4414a60a4d0d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c35c483fe04ed5ed44d03176d4bc70f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9833aa7efff793f320c",
+    "url": "/static/css/2.7fca699e.chunk.css"
+  },
+  {
+    "revision": "e1d9af41790919648ecf",
+    "url": "/static/css/main.e6c1da81.chunk.css"
+  },
+  {
+    "revision": "d9833aa7efff793f320c",
+    "url": "/static/js/2.b675339a.chunk.js"
+  },
+  {
+    "revision": "aacaa6d0203096b46e6921804d6c52f3",
+    "url": "/static/js/2.b675339a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1d9af41790919648ecf",
+    "url": "/static/js/main.f528f88c.chunk.js"
+  },
+  {
+    "revision": "9b65bac24efa50f8b7ca",
+    "url": "/static/js/runtime-main.c44b5c64.js"
+  }
+]);
